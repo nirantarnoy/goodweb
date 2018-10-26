c7fe270780d9ae8940fe44df65e1dc3316879baf
tarlektarnoy <niran_tarlek@hotmail.com>
2018-10-26 15:53:46 +0700

    First commit

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -10,7 +10,58 @@ use yii\widgets\Breadcrumbs;
 use frontend\assets\AppAsset;
 use common\widgets\Alert;
 
+Yii::$app->name="Goodoper";
+$this->title = "Goodoper";
+
 AppAsset::register($this);
+$this->registerCss('
+     body{
+                font-family: "Cloud-Light";
+                font-size: 16px;
+            }
+     ul.service-item{
+       list-style-type: none;
+       text-align: center;
+       display: inline-table;
+     }
+     ul.service-item >li{
+        float: left;
+        font-size: 18px;
+        padding-left: 5px;
+        padding-right: 5px;
+        width: 33%;
+     }
+     .about-text {
+        text-align: center;
+        background-color: #c7ddef;
+        padding-top : 20px;
+        padding-bottom: 100px;
+    }
+    .good-contact{
+        margin-bottom: 20px;
+    }
+    #myBtn {
+      display: none;
+      position: fixed;
+      bottom: 20px;
+      right: 30px;
+      z-index: 99;
+      font-size: 18px;
+      border: none;
+      outline: none;
+      background-color: #2aabd2;
+      color: white;
+      cursor: pointer;
+      padding: 15px;
+      border-radius: 4px;
+    }
+
+    #myBtn:hover {
+        background-color: #555;
+    }
+
+');
+
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
@@ -32,17 +83,20 @@ AppAsset::register($this);
         'brandLabel' => Yii::$app->name,
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
+            'class' => 'nav navbar-fixed-top',
         ],
     ]);
     $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
+        ['label' => 'หน้าแรก', 'options'=>['id'=>'nav-home','href'=>'']],
+        ['label' => 'บริการ', 'options'=>['id'=>'nav-service','href'=>'#good-service']],
+        ['label' => 'สินค้า', 'url' => ['/site/index']],
+        ['label' => 'ผลงาน', 'options'=>['id'=>'nav-port','href'=>'#good-port']],
+        ['label' => 'เกี่ยวกับเรา', 'options'=>['id'=>'nav-about','href'=>'#good-about']],
+        ['label' => 'ติดต่อเรา','options'=>['id'=>'nav-contact','href'=>'#good-contact']],
     ];
     if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+      //  $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
+      //  $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
     } else {
         $menuItems[] = '<li>'
             . Html::beginForm(['/site/logout'], 'post')
@@ -60,20 +114,20 @@ AppAsset::register($this);
     NavBar::end();
     ?>
 
-    <div class="container">
+<!--    <div class="container">-->
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
         <?= Alert::widget() ?>
         <?= $content ?>
-    </div>
+<!--    </div>-->
 </div>
 
 <footer class="footer">
     <div class="container">
         <p class="pull-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
 
-        <p class="pull-right"><?= Yii::powered() ?></p>
+        <p class="pull-right"><?php //echo Yii::powered() ?></p>
     </div>
 </footer>
 
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,53 +1,250 @@
 <?php
 
 /* @var $this yii\web\View */
+use kv4nt\owlcarousel\OwlCarouselWidget;
 
-$this->title = 'My Yii Application';
+$this->title = 'goodoper';
 ?>
 <div class="site-index">
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
+    <div class="jumbotron" style="background: #2aabd2;width: 100%; ">
+        <h1>บริการออกแบบและพัฒนาระบบงานทั่วไป</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead" style="color: #ffffff">ระบบซื้อขาย ระบบสต๊อกสินค้า ระบบงานซ่อมบำรุง ฯลฯ</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-warning" href="#">ทดลองใช้งาน</a></p>
     </div>
 
+
     <div class="body-content">
+        <div class="container">
+            <div class="row" id="good-service">
+                <div class="col-lg-12">
+                    <div class="good-service">
+                        <div style="text-align: center">
+                            <h2>บริการของเรา</h2>
+                        </div>
+                        <div style="text-align: center">
+<!--                            <ul class="service-item">-->
+<!--                                <li>-->
+<!---->
+<!--                                            <div class="thumbnail">-->
+<!--<!--                                                <img src="..." alt="...">-->
+<!--                                                <h1><i class="glyphicon glyphicon-cloud-download"></i></h1>-->
+<!--                                                <div class="caption">-->
+<!--                                                    <h3>ระบบงานบน Cloud</h3>-->
+<!--                                                    <p> ระบบบริหารงานซื้อ-ขาย ซ่อมบำรุงบน Cloud <br>หมดปัญหาเรื่องทรัพยากรไม่เพียงพอ</p>-->
+<!--                                                    <p><a href="#" class="btn btn-primary" role="button">รายละเอียด</a></p>-->
+<!--                                                </div>-->
+<!--                                            </div>-->
+<!--                                </li>-->
+<!--                                <li>-->
+<!--                                    <div class="thumbnail">-->
+<!--                                        <h1><i class="glyphicon glyphicon-th-large"></i></h1>-->
+<!--                                        <div class="caption">-->
+<!--                                            <h3>ออกแบบและพัฒนาระบบตามความต้องการ</h3>-->
+<!--                                            <p>นำความต้องการของลูกค้ามาวิเคราะห์และออกแบบระบบ<br />ให้ตรงตามความต้องการ พร้อมส่งมอบระบบที่มีคุณภาพ</p>-->
+<!--                                            <p><a href="#" class="btn btn-primary" role="button">รายละเอียด</a></p>-->
+<!--                                        </div>-->
+<!--                                    </div>-->
+<!--                                </li>-->
+<!--                                <li>-->
+<!--                                    <div class="thumbnail">-->
+<!--                                        <h1><i class="glyphicon glyphicon-cog"></i></h1>-->
+<!--                                        <div class="caption">-->
+<!--                                            <h3>Implement and customize</h3>-->
+<!--                                            <p> จัดหาระบบสารสนเทศที่เหมาะสมสำหรับองค์กร <br >รวมถึงการปรับแต่งระบบเพิ่มเติม เช่น Form,Report</p>-->
+<!--                                            <p><a href="#" class="btn btn-primary" role="button">รายละเอียด</a></p>-->
+<!--                                        </div>-->
+<!--                                    </div>-->
+<!--                                </li>-->
+<!--                            </ul>-->
+                            <div class="row">
+                                <div class="col-lg-4">
 
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+                                    <div class="thumbnail">
+                                        <!--                                                <img src="..." alt="...">-->
+                                        <h1><i class="glyphicon glyphicon-cloud-download"></i></h1>
+                                        <div class="caption">
+                                            <h3 style="color: #2b669a">ระบบงานบน Cloud</h3>
+                                            <p> ระบบบริหารงานซื้อ-ขาย ซ่อมบำรุงบน Cloud <br>หมดปัญหาเรื่องทรัพยากรไม่เพียงพอ</p>
+                                            <p><a href="#" class="btn btn-primary" role="button">รายละเอียด</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4">
+                                    <div class="thumbnail">
+                                        <h1><i class="glyphicon glyphicon-th-large"></i></h1>
+                                        <div class="caption">
+                                            <h3 style="color: #2b669a">ออกแบบและพัฒนาระบบตามความต้องการ</h3>
+                                            <p>นำความต้องการของลูกค้ามาวิเคราะห์และออกแบบระบบ<br />ให้ตรงตามความต้องการ พร้อมส่งมอบระบบที่มีคุณภาพ</p>
+                                            <p><a href="#" class="btn btn-primary" role="button">รายละเอียด</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4">
+                                    <div class="thumbnail">
+                                        <h1><i class="glyphicon glyphicon-cog"></i></h1>
+                                        <div class="caption">
+                                            <h3 style="color: #2b669a">Implement and customize</h3>
+                                            <p> จัดหาระบบสารสนเทศที่เหมาะสมสำหรับองค์กร <br >รวมถึงการปรับแต่งระบบเพิ่มเติม เช่น Form,Report</p>
+                                            <p><a href="#" class="btn btn-primary" role="button">รายละเอียด</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                    </div>
+                </div>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="good-portfolio" id="good-port">
+                        <div style="text-align: center">
+                            <h2>ตัวอย่างผลงาน</h2>
+                        </div>
+                        <div style="text-align: center">
+                            <?php
+                            OwlCarouselWidget::begin([
+                                'container' => 'div',
+                                'containerOptions' => [
+                                    'id' => 'container-id',
+                                    'class' => 'container-class'
+                                ],
+                                'pluginOptions'    => [
+                                    'autoplay'          => true,
+                                    'autoplayTimeout'   => 3000,
+                                    'items'             => 3,
+                                    'loop'              => true,
+                                    'itemsDesktop'      => [1199, 3],
+                                    'itemsDesktopSmall' => [979, 3]
+                                ]
+                            ]);
+                            ?>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                            <div class="item-class"><img src="../web/images/client_1.png" alt="Image 1"></div>
+                            <div class="item-class"><img src="../web/images/client_2.jpg" alt="Image 2"></div>
+                            <div class="item-class"><img src="../web/images/client_3.jpg" alt="Image 3"></div>
+                            <div class="item-class"><img src="../web/images/client_4.png" alt="Image 4"></div>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                            <?php OwlCarouselWidget::end(); ?>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                        </div>
+
+                    </div>
+                </div>
             </div>
         </div>
 
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="good-about" id="good-about">
+                        <div style="text-align: center">
+                            <h2>เกี่ยวกับเรา</h2>
+                        </div>
+                        <div class="about-text" style="text-align: center">
+                           <div class="row">
+                               <div class="col-lg-12">
+                                   <div class="container">
+                                       <p><h3>goodoper (กู๊ดดอปเปอร์) ให้บริการออกแบบและพัฒนาระบบบริหารจัดการภายในองกร ห้างร้าน บริษัท โรงงานอุตสาหกรรมทั้งขนาดเล็กและขนาดใหญ่ ด้วยประสบการณ์การทำงานด้านการพัฒนาและ implement ระบบมาเป็นระยะเวลาไม่น้อยกว่า 9 ปี
+                                           มีความเข้าใจในแต่ละกระบวนการทำงาน พร้อมที่จะออกแบบและพัฒนาระบบที่มีคุณภาพให้กับลูกค้าทุกท่านพึงพอใจและตรงความต้องการ</h3></p>
+                                   </div>
+
+                               </div>
+                           </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div id="good-contact">
+                        <div style="text-align: center">
+                            <h2>ติดต่อเรา</h2>
+                        </div>
+                        <div style="text-align: center">
+                           <div class="row">
+                               <div class="col-lg-12">
+                                   <p><h4>69/71 หมู่ 2 ถนนเพชรเกษม ต.ไร่ขิง อ.สามพราน จ.นครปฐม 73210</h4></p>
+                               </div>
+                           </div>
+                             <div class="row">
+                                 <div class="col-lg-12" style="text-align: center">
+
+                                         <a href="#" target="_blank" class="">
+                                             <img src="../web/images/mobile.png" width="10%" alt="...">
+
+                                         </a>
+                                         <h4><b>086 006 3601 , 088 769 2818</b></h4>
+
+                                 </div>
+                             </div>
+                            <br>
+                                    <div class="row">
+
+                                        <div class="col-lg-12">
+                                            <a href="https://www.facebook.com/Goodoper-1033800079972006/" target="_blank">
+                                                <img src="../web/images/facebook.png" width="5%" alt="">
+                                            </a>
+                                            <a href="#" class="" target="_blank">
+                                                <img src="../web/images/line.png" width="4%">
+                                            </a>
+                                            <a href="#" target="_blank" class="">
+                                                <img src="../web/images/google+.png" width="5%">
+                                            </a>
+                                        </div>
+                                    </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
     </div>
+    <button onclick="topFunction()" id="myBtn" title="Go to top">กลับขึ้นข้างบน</button>
 </div>
+<?php
+$js =<<<JS
+   $(function() {
+     window.onscroll = function() {scrollFunction()};
+     
+     $("#nav-contact,#nav-about,#nav-port,#nav-service").click(function() {
+         //$(this).preventDefault();
+         var aid = $(this).attr("href");
+         //alert(aid);
+         $('html,body').animate({scrollTop: $(aid).offset().top -100},'slow');
+     });
+     $("#nav-home").click(function(){
+        var aid = ".site-index"
+        // alert(aid);
+         $('html,body').animate({scrollTop: $(aid).offset().top -100},'slow');
+     });
+   });
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            document.getElementById("myBtn").style.display = "block";
+        } else {
+            document.getElementById("myBtn").style.display = "none";
+        }
+    }
+
+    function topFunction() {
+      //  document.body.scrollTop = 0;
+      //  document.documentElement.scrollTop = 0;
+       var aid = ".site-index"
+        // alert(aid);
+         $('html,body').animate({scrollTop: $(aid).offset().top -100},'slow');
+    }
+JS;
+
+$this->registerJs($js,static::POS_END);
+?>
